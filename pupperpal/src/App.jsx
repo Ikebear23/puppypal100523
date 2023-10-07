1bbc17c78abb38bd8b95b5f11426d7de226465d0
isaac <isaacsmith2120@gmail.com>
2023-10-07 15:42:03 -0500

    finishing  touches

--- pupperpal/src/App.jsx
+++ pupperpal/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [pupId, setPupid] = useState(null);
+  const featuredpup = puppies.find((pup) => pup.id === pupId);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setPupid(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      {pupId && (
+        <div>
+          <h2>{featuredpup.name}</h2>
+          <p>{featuredpup.email}</p>
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
